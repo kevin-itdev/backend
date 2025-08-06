e1575693bb7fa9da2e0e4355c8bd341bebb6ea4c
kevin-itdev <128466750+kevin-itdev@users.noreply.github.com>
2025-08-06 15:15:06 +0900

    Add files via upload

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
-console.log('v2025/07/10');
+console.log('v2025/08/05');
 const express = require('express');
 const cors = require('cors');
 require('dotenv').config();
 const http = require('http');
+const { RateLimiterMemory } = require('rate-limiter-flexible');
 const { Server } = require('socket.io');
+const { OAuth2Client } = require('google-auth-library');
 const { connectToLightSpeed, closePosLightSpeed, openOrdLightSpeed } = require('./lightspeed');
 const { connectToSilex, closePosSilex, openOrdSilex, stopRecurringPortfolioUpdates } = require('./silex');
 
@@ -16,12 +18,13 @@ app.use(cors({
 app.use(express.json());
 
 const server = http.createServer(app);
-const io = new Server(server, {cors: { 
-    origin: allowedOrigins.filter(Boolean),
-    methods: ["GET", "POST"] 
-}});
-
-
+const io = new Server(server, {cors: { origin: allowedOrigins.filter(Boolean), methods: ["GET", "POST"] },
+    pingInterval: 15000, // Send a ping every 15 seconds
+    pingTimeout: 10000,  // Disconnect if no pong received within 10 seconds
+});
+const client = new OAuth2Client(process.env.GOOGLE_CLIENT_ID);
+const rateLimiter = new RateLimiterMemory({ points: 5, duration: 60 }); // Rate limiter middleware: 5 requests per 60 seconds
+let verifyId;
 
 
 //Connect to our front-end
@@ -29,6 +32,17 @@ io.on('connection', (socket) => {
 
     console.log('A user connected', socket.id);
     let selectedBroker, lightSpeedWebSocket, silexReconnectTimeoutId, silexToken, silexAccId;
+
+    socket.on('login', async (userId) => { //Google login
+
+        console.log(`Google login for user: ${socket.id} and google Id: ${userId}`);
+        if(verifyId == 'kevinbkeena@gmail.com' || verifyId == 'david@spxmgmt.com') {
+        } else {
+            socket.emit('verifyId', 'Wrong username!');
+            socket.disconnect();
+        }
+    });
+
 
     socket.on('broker', async(brokerName) => {
 
@@ -110,6 +124,43 @@ io.on('connection', (socket) => {
             clearTimeout(silexReconnectTimeoutId);
     });
 });
+
+
+async function verifyGoogleToken(credential) {
+    try {
+        const ticket = await client.verifyIdToken({
+            idToken: credential,
+            audience: process.env.GOOGLE_CLIENT_ID,
+        });
+        const payload = ticket.getPayload();
+        const userId = payload.sub;
+        const email = payload.email;
+        const name = payload.name;
+
+        return { userId, email, name };
+    } catch (error) {
+        console.error('Google token verification failed:', error);
+        return null;
+    }
+}
+
+app.post('/api/google-login', async (req, res) => {
+    const { credential } = req.body;
+
+    if (!credential)
+        return res.status(400).json({ message: 'No credential provided' });
+
+    const userData = await verifyGoogleToken(credential);
+    console.log('userData', userData);
+    verifyId = userData.email;
+    if (userData)
+        return res.json({ message: 'Google login successful', user: userData });
+    else {
+        return res.status(401).json({ message: 'Invalid Google token' });
+    }
+});
+
+
 
 app.get('/', (req, res) => { res.send('Server is up and running!'); });
 server.listen(3001, () => { console.log('Listening on port:3001'); });
